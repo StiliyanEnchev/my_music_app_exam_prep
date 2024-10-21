887e239542a28a80a856748923965703a09eeac0
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-10-21 19:20:54 +0300

    Initial commit

---
+++ profiles/models.py
@@ -0,0 +1,28 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator
+from django.db import models
+from django.template.defaultfilters import slugify
+
+
+# Create your models here.
+
+def alpha_numeric_validator(value: str):
+    if value != slugify(value):
+        raise ValidationError('Ensure this value contains only letters, numbers, and underscore')
+
+
+class Profile(models.Model):
+
+    username = models.CharField(
+        max_length=15,
+        validators=[MinLengthValidator(2),
+                    alpha_numeric_validator],
+    )
+
+    email = models.EmailField()
+
+    age = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+    )
+
